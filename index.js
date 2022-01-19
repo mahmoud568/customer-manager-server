8761f69d9eaa0f01911d64fa50efca8ab81297fa
mahmoud abdelwahab <mahmoudabdelwahab4242@gmail.com>
2022-01-19 21:31:09 +0200

    initial

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const express = require('express');
 const app = express();
 
 
-app.get('/', (req, res) => res.send('hello wrold'))
+app.get('/', (req, res) => res.json({
+    status: "error",
+  }))
 
 app.listen(process.env.PORT || 3000)
