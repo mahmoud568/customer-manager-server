a7124cbf70b1fcfc64a0baf31273bde97b9c44e9
mahmoud abdelwahab <mahmoudabdelwahab4242@gmail.com>
2022-01-19 21:41:42 +0200

    initial

--- index.js
+++ index.js
@@ -4,9 +4,9 @@
 const express = require("express");
 const app = express();
 const cors = require("cors");
-// app.use(cors());
+app.use(cors());
 
-// const bodyParser = require("body-parser");
+const bodyParser = require("body-parser");
 // app.use(bodyParser.json());
 // app.use(bodyParser.urlencoded({ extended: true }));
 
